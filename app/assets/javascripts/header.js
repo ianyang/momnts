71e6e404ba2d81496842ae5da47e799277649618
Ian Yang <ianyangyi@gmail.com>
2013-08-15 09:34:40 -0700

    more layouts

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -1,13 +1,14 @@
 $(function(){
 
   var $itemContent = $(".popout");
+
   $(".open-popout").click(function(e){
     e.preventDefault();
     $itemContent.animate({'margin-left': 0}, 200);
   });
 
-  $(".popout").click(function(e){
-    $itemContent.animate({'margin-left': '-50%'}, 200);
+  $(".popout").click(function(){
+    $itemContent.animate({'margin-left': '-90%'}, 200);
   });
 
 });
