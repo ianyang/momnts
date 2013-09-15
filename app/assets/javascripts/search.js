64dc01f0364129453af163e7cb37f2706f6a0af4
Ian Yang <ianyangyi@gmail.com>
2013-09-15 12:13:55 -0700

    location based ajax

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,21 +1,38 @@
 $(document).ready(function(){
   "use strict";
 
+  var lat;
+  var lng;
+
+  function events() {
+    $.ajax({
+      url: "/all_events",
+      method: "get",
+      dataType: "json",
+      data: {"lat": lat, "lng": lng},
+      success: function(data) {
+        $.each(data, function(x) {
+          $('.today-container .result').append(data[x]);
+          console.log(data[x]);
+        });
+        $('.loading').css('display','none');
+        $('.today-container').fadeIn();
+        $('.tomorrow-container').fadeIn();
+      }
+    });
+  }
+
+  if (!navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      lat = position.coords.latitude;
+      lng = position.coords.longitude;
+      events();
+    });
+  } else {
+    lat = '37.7750';
+    lng = '-122.4183';
+    events();
+  }
+
+
 });
-
-
-
-
-// function searchEvent() {
-//     $.ajax(
-//     '/all_events',
-//     { success: function (data) {
-
-//       data for each
-
-//     }
-//     }
-//   )
-// });
-// }
-
